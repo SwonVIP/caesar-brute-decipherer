cfd9cd33ae5b3cc94b1ff07f519d53ffe4976aa2
Sven Scheffel <mail@scheffel.ch>
2019-12-30 16:27:27 +0100

    Added basic layout and function structure

---
+++ deciphe.js
@@ -0,0 +1,20 @@
+"use strict";
+
+startDeciphe = () => {
+    const inputValue = document.getElementById("inputField").value;
+    incDeciphe(inputValue)
+}
+
+incDeciphe = (input) => {
+
+    let decipheArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    for(key = 1;key < 27;key++)
+    {
+        
+    }
+}
+
+decipheCurrentKey = (input, key) =>
+{
+
+}
