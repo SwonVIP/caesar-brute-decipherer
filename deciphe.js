d0cda24f0aae34092aa151a05e2379b07529d4b6
Sven Scheffel <mail@scheffel.ch>
2020-01-02 17:04:51 +0100

    Added decipher logic

--- deciphe.js
+++ deciphe.js
@@ -1,20 +1,39 @@
 "use strict";
 
-startDeciphe = () => {
-    const inputValue = document.getElementById("inputField").value;
-    incDeciphe(inputValue)
+function startDeciphe(){
+    const inputValue = document.getElementById("inputField").value.toUpperCase();
+    document.getElementById("outputField").value = incDeciphe(inputValue);
 }
 
-incDeciphe = (input) => {
+function incDeciphe(input){
 
-    let decipheArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    for(key = 1;key < 27;key++)
+    let targetString = '';
+    //duplicate array to allow transformation based on high key values
+    let decipheArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    for(let key = 1;key <= 26;key++)
     {
-        
+        targetString = targetString + '\r\n Key' + key + ': ';
+
+        for(let i=0;i<=input.length;i++)
+        {
+            //filter placeholder chars out
+            if(input.charAt(i) != ' ' && input.charAt(i) != ',' && input.charAt(i) != '.' && input.charAt(i) != '-')
+            {
+                //only iterate through the first half of the array minus offset
+                for(let z=0;z<=((decipheArray.length/2)-1);z++)
+                {
+                    if(input.charAt(i) == decipheArray[z])
+                    {
+                        //deciphered char will match the char in the array plus the key value
+                        targetString = targetString + decipheArray[z+key];   
+                    }
+                }
+            }
+            else
+            {
+                targetString = targetString + ' ';
+            }
+        }
     }
-}
-
-decipheCurrentKey = (input, key) =>
-{
-
+    return targetString;
 }
